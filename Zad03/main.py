607910e70f7a5f1b99119ba9d1521d7d679b1899
Jakub Woźniak <jwozniak@inf.ug.edu.pl>
2023-11-09 14:12:17 +0100

    Zad03

---
+++ Zad03/main.py
@@ -0,0 +1,37 @@
+import os
+from multiprocessing import Process, Queue
+
+
+def process_file(file_name, queue):
+    with open(os.path.join("./input/", file_name), 'r') as file:
+
+        for line in file:
+            if line.startswith("\\input{"):
+                subfile = line.split("{")[1].split("}")[0]
+                p = Process(target=process_file, args=(subfile, queue))
+                p.start()
+                p.join()
+            else:
+                queue.put(line)
+    print(queue)
+
+
+def count_occurences(start_file, word):
+    queue = Queue()
+    p = Process(target=process_file, args=(start_file, queue))
+    p.start()
+    p.join()
+
+    count = 0
+    while not queue.empty():
+        line = queue.get()
+        count += line.split().count(word)
+
+    return count
+
+
+if __name__ == "__main__":
+    start_file = "mainInput.txt"
+    target_word = "Stoi"
+    result = count_occurences(start_file, target_word)
+    print(f"Word '{target_word}' occurs {result} times in {start_file}")
