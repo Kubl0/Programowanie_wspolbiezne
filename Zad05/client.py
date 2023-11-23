8553d2de7dfe558835846cc552df3723e3af9d22
Kublo <jakubwozniak0611@gmail.com>
2023-11-23 13:19:51 +0100

    Zad05

--- Zad05/client.py
+++ Zad05/client.py
@@ -1,32 +1,23 @@
 import os
 import sysv_ipc
 
-# Klucz dla kolejki wejściowej serwera
 SERVER_INPUT_KEY = 1234
 
-# Klucz dla kolejki wyjściowej serwera
 SERVER_OUTPUT_KEY = 5678
 
-# PID klienta
 client_pid = os.getpid()
 
-# Utwórz kolejki wejściową i wyjściową klienta
 input_queue = sysv_ipc.MessageQueue(client_pid, sysv_ipc.IPC_CREAT)
 output_queue = sysv_ipc.MessageQueue(client_pid, sysv_ipc.IPC_CREAT)
 
-# Słowo do przetłumaczenia
 word_to_translate = "cat"
 
-# Wyślij zapytanie do serwera
 input_queue.send(str(client_pid).encode())
 input_queue.send(word_to_translate.encode())
 
-# Odbierz odpowiedź od serwera
 response, msg_type = output_queue.receive(type=client_pid)
 
-# Wypisz odpowiedź
 print(f"Odpowiedź od serwera: {response.decode().split(':')[-1]}")
 
-# Usuń kolejki klienta
 input_queue.remove()
 output_queue.remove()
--- Zad05/server.py
+++ Zad05/server.py
@@ -2,17 +2,13 @@ import sysv_ipc
 import os
 import time
 
-# Klucz dla kolejki wejściowej serwera
 SERVER_INPUT_KEY = 1234
 
-# Klucz dla kolejki wyjściowej serwera
 SERVER_OUTPUT_KEY = 5678
 
-# Utwórz kolejki wejściową i wyjściową serwera
 input_queue = sysv_ipc.MessageQueue(SERVER_INPUT_KEY, sysv_ipc.IPC_CREAT)
 output_queue = sysv_ipc.MessageQueue(SERVER_OUTPUT_KEY, sysv_ipc.IPC_CREAT)
 
-# Słownik angielsko-polski
 dictionary = {
     "cat": "kot",
     "dog": "pies",
@@ -21,27 +17,20 @@ dictionary = {
 
 while True:
     try:
-        # Odbierz zapytanie z dowolnego klienta
         message, msg_type = input_queue.receive(type=0)
 
-        # Pobierz PID klienta z treści komunikatu
         client_pid = int(message.decode())
 
-        # Odczytaj słowo z komunikatu
         word = input_queue.receive(type=client_pid)[0].decode()
 
-        # Szukaj tłumaczenia słowa w słowniku
         translation = dictionary.get(word, "Nie znam takiego słowa")
 
-        # Odpowiedz klientowi
         response_msg = f"{os.getpid()}:{translation}"
         output_queue.send(response_msg.encode(), type=client_pid)
 
-        # Symuluj opóźnienie w serwerze (do testowania)
         time.sleep(1)
 
     except KeyboardInterrupt:
-        # Obsłuż przerwanie z klawiatury
         print("Serwer zakończony")
         input_queue.remove()
         output_queue.remove()
