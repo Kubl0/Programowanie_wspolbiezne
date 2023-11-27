d0afc1fae540a9fc1fecf0a301326668a1ec058b
Kublo <jakubwozniak0611@gmail.com>
2023-11-27 12:48:48 +0100

    Small fixes

--- Zad04/server.py
+++ Zad04/server.py
@@ -1,26 +1,22 @@
 import os
 import json
 import threading
-import subprocess
+import time
 
-# Klasa reprezentująca bazę danych postaci
 class Database:
     def __init__(self):
-        # Przykładowa statyczna baza danych
         self.data = {
             1: "Kowalski",
             2: "Nowak",
             3: "Smith"
         }
 
-# Klasa obsługująca komunikację serwera z klientem
 class Server:
     def __init__(self, database, input_queue_path):
         self.database = database
         self.input_queue_path = input_queue_path
 
     def process_request(self, request):
-        # Przetwarzanie zapytania od klienta
         try:
             request_data = json.loads(request)
             client_queue_path = request_data['client_queue']
@@ -28,36 +24,43 @@ class Server:
 
             response = self.database.data.get(client_id, "Nie ma")
 
-            # Odpowiedź do klienta
+            time.sleep(10)
             with open(client_queue_path, 'w') as f:
                 f.write(json.dumps([response]))
+
+            with open(self.input_queue_path, 'r') as f:
+                lines = f.readlines()
+
+            with open(self.input_queue_path, 'w') as f:
+                if lines:
+                    f.writelines(lines[1:])
+
         except Exception as e:
             print(f"Błąd przetwarzania zapytania: {e}")
 
     def start(self):
         while True:
-            # Odbieranie zapytań z kolejki wejściowej
             with open(self.input_queue_path, 'r') as f:
-                request = f.read().strip()
+                lines = f.readlines()
+
+            for line in lines:
+                request = line.strip()
                 if request:
+                    print(f"\nPrzetwarzanie zapytania: {request}")
                     self.process_request(request)
+                    break
+
+            time.sleep(1)
 
 if __name__ == "__main__":
-    # Inicjalizacja serwera
     database = Database()
     input_queue_path = "server_input_queue.txt"
 
-    # Utworzenie pliku kolejki wejściowej serwera
     if not os.path.exists(input_queue_path):
         open(input_queue_path, 'w').close()
 
-    # Uruchomienie serwera w osobnym wątku
     server = Server(database, input_queue_path)
     server_thread = threading.Thread(target=server.start)
     server_thread.start()
 
-    # Odbieranie zapytań od klienta
-    while True:
-        subprocess.run(["type", input_queue_path], shell=True)
-
-
+    input("Wciśnij Enter, aby zakończyć program...")
