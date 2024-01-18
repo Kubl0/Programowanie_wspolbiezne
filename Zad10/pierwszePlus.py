267bf127493e316abbc2dcd05f84832a5eda8725
jwozniak <jakubwozniak0611@gmail.com>
2024-01-18 12:39:46 +0100

    Fixed Lab10

--- Zad10/pierwszePlus.py
+++ Zad10/pierwszePlus.py
@@ -2,6 +2,7 @@ import math
 import multiprocessing
 import time
 
+
 def is_prime(num):
     if num < 2:
         return False
@@ -9,6 +10,7 @@ def is_prime(num):
         if num % i == 0:
             return False
     return True
+
 
 def sequential_primes(start, end):
     primes = []
@@ -23,11 +25,13 @@ def sequential_primes(start, end):
 
     return primes
 
+
 def partial_check_prime(num):
     for i in range(2, int(math.sqrt(num)) + 1):
         if num % i == 0:
             return False
     return True
+
 
 def parallel_primes(start, end, num_processes, shared_data):
     global sqrt_end_primes
@@ -42,9 +46,11 @@ def parallel_primes(start, end, num_processes, shared_data):
 
     return primes
 
+
 def init_shared_data(data):
     global sqrt_end_primes
     sqrt_end_primes = data
+
 
 def partial_check_prime_parallel(num):
     for prime in sqrt_end_primes:
@@ -52,13 +58,13 @@ def partial_check_prime_parallel(num):
             return False
     return True
 
-def main():
-    start_time = time.time()
 
+def main():
     start = 1
     end = 10000
     num_processes = 4
 
+    start_time = time.time()
     sequential_result = sequential_primes(start, end)
     sequential_time = time.time() - start_time
 
@@ -66,14 +72,12 @@ def main():
     parallel_result = parallel_primes(start, end, num_processes, sequential_result[:])
     parallel_time = time.time() - start_time
 
-    print("Sequential primes:", sequential_result)
-    print("Parallel primes:", parallel_result)
+    # print("Sequential primes:", sequential_result)
+    # print("Parallel primes:", parallel_result)
 
     print("Sequential time:", sequential_time)
     print("Parallel time:", parallel_time)
 
-if __name__ == "__main__":
-    main()
 
 if __name__ == "__main__":
     main()
